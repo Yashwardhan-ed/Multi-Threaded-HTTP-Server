7c6a00c0769956739360febaf5239ae37e7d618c
Yashwardhan-ed <180419645+Yashwardhan-ed@users.noreply.github.com>
2025-09-26 23:44:31 +0530

    resolve path function for path verification

--- server.py
+++ server.py
@@ -1,31 +1,34 @@
 import socket
 import concurrent.futures
 import os
+import sys
 import time
 import threading
+from urllib.parse import unquote
 
 
 interface = "127.0.0.1"
 port = 8080
+resources = "resources"
 client_que = []
 clients = 0
 lock = threading.Lock()
 
-def start_server(port, interface, maxPoolSize=10):
+def start_server(port, interface, resource_dir, maxPoolSize=10):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind((interface, port))
     sock.listen()
     with concurrent.futures.ThreadPoolExecutor(max_workers=maxPoolSize) as executor:
         conn, addr = sock.accept()
         no_of_clients+=1
-        executor.submit(handle_client_connection, conn, addr, executor)
+        executor.submit(handle_client_connection, conn, addr, executor, resource_dir)
     if clients >= maxPoolSize:
         print("Thread pool saturated, queing connection")
         client_que.append((conn, addr))
 
 
 
-def handle_client_connection(conn, addr, executor):
+def handle_client_connection(conn, addr, executor, resources_dir):
     client_ip, client_port = addr
     print(f"Connection from {client_ip} {client_port}")
     try: 
@@ -51,6 +54,18 @@ def handle_client_connection(conn, addr, executor):
                 response = make_response(505, "Version not Supported", error_page("505", "Version not supported", "Wrong HTTP version"))
                 conn.sendall(response)
                 print(" --> 505 Version not supported")
+            # resolve the path 
+            ok, error_or_path = resolve_path(resource_dir, path)
+            if ok == False:
+                status_code = error_or_path.split(" ")[0]
+                response = ""
+                if status_code == "403":
+                    response = make_response(status_code, "Forbidden", error_page(status_code, "Forbidden", "Forbidden Path"))
+                else:
+                    response = make_response(status_code, "Bad Request", error_page(status_code, "Bad Request", "Malformed Request"))
+                conn.sendall(response)
+                print(f" --> {error_or_path}")
+                return
             # if method.upper() == "GET":
 
             # if method.upper() == "POST":
@@ -64,11 +79,31 @@ def handle_client_connection(conn, addr, executor):
         no_of_clients-=1
         if not client_que.empty():
             client = client_que.pop()
-            exec
+            next_conn, next_addr = client
+            executor.submit(handle_client_connection, next_conn, next_addr, executor)
         lock.release()
 
             
+def resolve_path(resource_dir, request_path):
+    path = request_path.split('?')[0] # separate path from query
+    path = path.unquote() # remove any special characters if they exist in the path
+    if path == "": # default empty path to /
+        path = "/"
+    if path == "/": # default / to /index.html page
+        path == "/index.html"
+    
+    # avoid using absolute paths (security risk)
+    if path.startswith('http://') or path.startswith('https://'):
+        return False, "400 Malformed Request path"
+    
+    requested_rel = path.lstrip() # remove any leading /
+    candidate_path = os.path.join(resource_dir, requested_rel) 
+    real_path = os.path.realpath(resource_dir)
+    real_prefix = real_path + os.path.sep
 
+    if not candidate_path.startswith(real_prefix):
+        return False, "403 Forbidden"
+    return True, "200 Ok"
 
 def make_response(status_code, reason, body_html, connection, extra_headers=None):
     if body_html == "":
@@ -102,4 +137,22 @@ def error_page(status_code, reason, detail):
             '''
 
 if __name__ == "__main__":
-    start_server()
+    PORT = port
+    INTERFACE = interface
+    threadpool_size = 10
+    # sys.argv takes input from command line
+    # sys.argv[0] is reserved for file name (e.g. server.py)
+    if len(sys.argv) >= 2:
+        try:
+            PORT = int(sys.argv[1])
+        except:
+            print(f"Invalid port Number. Using Default {PORT}")
+        if len(sys.argv) >= 3:
+            INTERFACE = int(sys.argv[2])
+        if len(sys.argv) >= 4:
+            threadpool_size = int(sys.argv[3])
+    
+    base_dir = os.path.dirname(os.path.realpath(__file__))
+    resource_dir = os.path.join(base_dir, resources)
+
+    start_server(PORT, INTERFACE, resource_dir, threadpool_size)
