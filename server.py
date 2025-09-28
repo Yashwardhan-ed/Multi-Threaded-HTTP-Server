9800a6753ac1ebfc9c43b72ec8e71e33cda35cf0
Yashwardhan-ed <180419645+Yashwardhan-ed@users.noreply.github.com>
2025-09-28 15:32:41 +0530

    Completed "GET" method + added comments

--- server.py
+++ server.py
@@ -12,7 +12,8 @@ port = 8080
 resources = "resources"
 client_que = []
 clients = 0
-lock = threading.Lock()
+# a lock is needed for maintaining client_que
+lock = threading.Lock() 
 
 def start_server(port, interface, resource_dir, maxPoolSize=10):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -33,6 +34,7 @@ def handle_client_connection(conn, addr, executor, resources_dir):
     print(f"Connection from {client_ip} {client_port}")
     try: 
         while True:
+            # recieve data and encode it
             raw_data = conn.recv(8192).decode("utf-8")
             if not raw_data:
                 break
@@ -50,25 +52,97 @@ def handle_client_connection(conn, addr, executor, resources_dir):
                 print(" --> 400 Bad Request")
                 return
             method, path, version = parts
+            # version check
             if version not in ("HTTP/1.1", "HTTP/1.0"):
                 response = make_response(505, "Version not Supported", error_page("505", "Version not supported", "Wrong HTTP version"))
                 conn.sendall(response)
                 print(" --> 505 Version not supported")
-            # resolve the path 
-            ok, error_or_path = resolve_path(resource_dir, path)
-            if ok == False:
-                status_code = error_or_path.split(" ")[0]
-                response = ""
-                if status_code == "403":
-                    response = make_response(status_code, "Forbidden", error_page(status_code, "Forbidden", "Forbidden Path"))
-                else:
-                    response = make_response(status_code, "Bad Request", error_page(status_code, "Bad Request", "Malformed Request"))
-                conn.sendall(response)
-                print(f" --> {error_or_path}")
-                return
-            # if method.upper() == "GET":
+            # process the response according to method
+            path = error_or_path
+            if method.upper() == "GET":
+                # resolve the path 
+                ok, error_or_path = resolve_path(resource_dir, path)
+                if not ok:
+                    response = make_response(403, "Forbidden", error_page("403", "Forbidden", "Forbidden Path"))
+                    conn.sendall(response)
+                    print(" --> 403 Forbidden")
+                if not os.path.exists(path) or not os.path.isfile(path):
+                    response = make_response(404, "Bad Request", error_page("404", "Bad Request", "Malformed Request"))
+                    conn.sendall(response)
+                    print(" --> 404 Not Found")
+                    return
+                try:
+                    if path.endswith(".html"):
+                        with open(path, "r") as f:
+                            data = f.read().encode("utf-8")
+                        response = make_response(200, "OK", data)   
+                        conn.sendall(response)
+                        print(" --> 200 OK")
+                    elif path.endswith((".png", ".jpeg", ".jpg", ".txt")):
+                        with open(path, "rb") as f:
+                            data = f.read()
+                        extra_headers = [
+                            "Content-Type: application/octet-stream",
+                            f'Content-Disposition: attachment; filename="{os.path.basename(path)}"',
+                        ]
+                        response = make_response(200, "OK", extra_headers=extra_headers)
+                        conn.sendall(response)
+                        print(" --> 200 OK")
+                except Exception:
+                    print(" --> 500 Internal Server Error")
+            elif method.upper() == "POST":
+                # Check the path
+                if path != "/uploads":
+                    response = make_response(404, "Not Found", error_page("404", "Not found", "Unvalid file path"))
+                    conn.sendall(response)
+                    print(" --> 404 Nof Found")
+                    return
+                # parse the header
+                headers = {}
+                for line in request_lines:
+                    if line.strip() == "":
+                        break
+                    if ":" in line:
+                        k, v = line.split(":", 1)
+                        headers[k.lower().strip()] = v.strip()
 
-            # if method.upper() == "POST":
+                # Validate Content-Type (should be application/json)
+                if headers.get("Content-Type", "") != "application/json":
+                    response = make_response(415, "Unsupported Media Type", error_page("415", "Unsupported Media Type", "This media type is not supported")
+                    conn.sendall(response)
+                    print(" --> 415 Unsupported Media Type")
+                    return
+
+                # Check Content-Length
+                if "Content-Length" not in headers:
+                    response = make_response(411, "Content-Length required", error_page("411", "Content-Length Required", "Content-Length not mentioned"))
+                    conn.sendall(response)
+                    con.sendall(response)
+                    print(" --> 411 Content-Length Required")
+                    return
+                try:
+                    content_length = int(headers["Content-Length"])
+                except:
+                    response = make_response(500, "Internal Server Error", "Internal Server Error")
+                    conn.senall(response)
+                    print(" --> 500 Internal Server Error")
+                    return
+
+                # Extract the body from the data
+                body = data.split("\r\n\r\n")[1].encode("utf-8")
+
+                # compare Content-Length with the body Length
+                if len(body) < content_length:
+                    response = make_response(400, "Bad Request", error_page("400", "Bad Request", "content and body length are not equal"))
+                    conn.sendall(response)
+                    print(" --> 400 Bad Request")
+                    return
+
+
+
+            else:
+                response = make_response(405, "Method Not Allowed", error_page(""))
+            
     except:
         response = make_response(500, "Internal Server Error", "Internal Server Error")
         conn.sendall(response)
